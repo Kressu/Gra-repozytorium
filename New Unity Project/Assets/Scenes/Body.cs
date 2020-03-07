590afac5646fa90b69d1b2938c6113d04fcca0ad
Kressu <58138125+Kressu@users.noreply.github.com>
2020-03-07 20:05:42 +0100

    v2
    wszystko dziala

---
+++ New Unity Project/Assets/Scenes/Body.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Body : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float playerSpeed = 3;
+    private Vector2 movment;
+    public float jumoForce = 5;
+    private bool canJump = false;
+    private bool ground = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movment.x = Input.GetAxisRaw("Horizontal");
+        movment.y = Input.GetAxisRaw("Vertical");
+        if (Input.GetButtonDown("jump")&& ground)
+        {
+            canJump = true;
+        }
+        
+    }
+
+     void FixedUpdate()
+    {
+        rb.velocity = new Vector2(movment.x *playerSpeed, rb.velocity.y);
+        if (canJump=true)
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            canJump = false;
+            ground = false;
+        }
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "ground")
+        {
+            ground = true;
+        }
+    }
+}
---
+++ New Unity Project/Assets/Scenes/playerCONTROLER.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerCONTROLER : MonoBehaviour
+{
+    public float playerSpeed = 3;
+    private Vector2 movment;
+    private SpriteRenderer m_SpriteRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_SpriteRenderer = GetComponent<SpriteRenderer>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movment.x = Input.GetAxisRaw("Horizontal");
+        movment.y = Input.GetAxisRaw("Vertical");
+        print(movment.x);
+        transform.position += new Vector3(playerSpeed * movment.x * Time.deltaTime, playerSpeed * movment.y * Time.deltaTime, 0);
+        if (movment.x < 0)
+        {
+            m_SpriteRenderer.flipX = false;
+        }
+        else if (movment.x > 0)
+        {
+            m_SpriteRenderer.flipX = true;
+        }
+    }
+}
