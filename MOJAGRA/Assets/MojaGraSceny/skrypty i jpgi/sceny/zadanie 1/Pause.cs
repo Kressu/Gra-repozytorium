c2791649b188fc3fce15839414586bfb31cac2e8
Kressu <58138125+Kressu@users.noreply.github.com>
2020-06-01 20:50:31 +0200

    zadanie 1

---
+++ MOJAGRA/Assets/MojaGraSceny/skrypty i jpgi/sceny/zadanie 1/Pause.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    
+
+    // Update is called once per frame
+    public void PouseToggle()
+
+    {
+        if(Time.timeScale == 1f)
+        {
+            Time.timeScale = 0f;
+                }
+        else
+        {
+            Time.timeScale = 1f;
+        }
+    }
+}
---
+++ MOJAGRA/Assets/MojaGraSceny/skrypty i jpgi/sceny/zadanie 1/PointC.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PointC : MonoBehaviour
+{
+    public Text pointText;
+    private int points = 0;
+    // Start is called before the first frame update
+  
+    // Update is called once per frame
+    void Update()
+    {
+        pointText.text = "SCORE:" + points.ToString(); 
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    { 
+        if(other.gameObject.tag == "Box")
+        {
+            points += 1;
+        }
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
